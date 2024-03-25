52b55226abc5aff38003779899e43b6333cc109f
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-03-25 08:48:05 -0600

    Update ClickEventHandler.java

--- src/main/java/net/reaper/ancientnature/event/ClickEventHandler.java
+++ src/main/java/net/reaper/ancientnature/event/ClickEventHandler.java
@@ -1,30 +1,27 @@
 package net.reaper.ancientnature.event;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.reaper.ancientnature.item.ModItems;
 
 @Mod.EventBusSubscriber(modid = "ancientnature", bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ClickEventHandler {
 
-    public static void onPlayerLeftClick(PlayerInteractEvent.RightClickItem event) {
-
-        ItemStack mainHandItem;
-        mainHandItem = event.getPlayer().getHeldItemMainhand();
-        ItemStack offHandItem;
-        offHandItem = event.getPlayer().getHeldItemOffhand();
-        if (!mainHandItem.isEmpty() && mainHandItem.getItem().getDescriptionId().equals("minecraft:brush") &&
-                !offHandItem.isEmpty() && offHandItem.getItem() == ModItems.CAMBRIAN_FOSSIL.get()) {
-
-            if (Math.random() < 0.3) {
-
-                 event.getPlayer().addItemStackToInventory(new ItemStack(ModItems.ANOMALOCARIS_FOSSIL.get()));
-            } else {
-                event.getPlayer().setHeldItem(event.getHand(), ItemStack.EMPTY);
+    @SubscribeEvent
+    public static void onPlayerRightClick(PlayerInteractEvent.RightClickItem event) {
+        if (event.getHand() == InteractionHand.MAIN_HAND) { // Check if main hand
+            if (event.getItemStack().getItem() == Items.BRUSH) { // Check if holding brush
+                if (event.getEntity().getOffhandItem().getItem() == ModItems.CAMBRIAN_FOSSIL.get()) { // Check if holding fossil in off hand
+                    if (Math.random() < 0.3) { // 30% chance
+                        event.getEntity().addItem(ModItems.ANOMALOCARIS_FOSSIL.get().getDefaultInstance()); // Give another fossil
+                    } else {
+                        event.getEntity().getOffhandItem().setCount(event.getEntity().getOffhandItem().getCount() - 1); // Decrease fossil count
+                    }
+                }
             }
         }
     }
 }
-
-
-
