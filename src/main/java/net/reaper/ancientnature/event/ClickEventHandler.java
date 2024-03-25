50ac4847234399896bf2acb0008dd638b240bd8b
Funado15 <juanjoromo2006@gmail.com>
2024-03-25 12:00:42 +0100

    Deleted CambrianFossilCleanProcedure.java Created ClickEventHandler.java

--- src/main/java/net/reaper/ancientnature/event/ClickEventHandler.java
+++ src/main/java/net/reaper/ancientnature/event/ClickEventHandler.java
@@ -6,21 +6,21 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.reaper.ancientnature.item.ModItems;
 
-@Mod.EventBusSubscriber(modid = "tu_mod_id", bus = Mod.EventBusSubscriber.Bus.FORGE)
+@Mod.EventBusSubscriber(modid = "ancientnature", bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ClickEventHandler {
 
     public static void onPlayerLeftClick(PlayerInteractEvent.RightClickItem event) {
-        // Verificar si el jugador hace clic izquierdo con un objeto en la mano principal
-        ItemStack heldItem = event.getPlayer().getHeldItemMainhand();
 
-        // Verificar si el jugador tiene el objeto de la brocha en la mano principal
-        if (!heldItem.isEmpty() && heldItem.getItem() == Items.BRUSH) {
-            // Aquí puedes implementar la lógica para dar otro objeto o hacer que el objeto de la brocha desaparezca
-            if (Math.random() < 0.5) { // 50% de probabilidad
-                // Dar otro objeto al jugador
+        ItemStack mainHandItem = event.getPlayer().getHeldItemMainhand();
+        ItemStack offHandItem = event.getPlayer().getHeldItemOffhand();
+
+        if (!mainHandItem.isEmpty() && mainHandItem.getItem().getRegistryName().toString().equals("minecraft:brush") &&
+                !offHandItem.isEmpty() && offHandItem.getItem() == ModItems.CAMBRIAN_FOSSIL.get()) {
+
+            if (Math.random() < 0.3) {
+
                 event.getPlayer().addItemStackToInventory(new ItemStack(ModItems.ANOMALOCARIS_FOSSIL.get()));
             } else {
-                // Hacer que el objeto de la brocha desaparezca
                 event.getPlayer().setHeldItem(event.getHand(), ItemStack.EMPTY);
             }
         }
