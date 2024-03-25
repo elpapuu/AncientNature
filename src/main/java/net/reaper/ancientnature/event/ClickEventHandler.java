cd57c8ff8732b08f3ad4089e8d21acca1c5db7df
Funado15 <juanjoromo2006@gmail.com>
2024-03-25 23:13:03 +0100

    Deleted CambrianFossilCleanProcedure.java Created ClickEventHandler.java

--- src/main/java/net/reaper/ancientnature/event/ClickEventHandler.java
+++ src/main/java/net/reaper/ancientnature/event/ClickEventHandler.java
@@ -1,7 +1,9 @@
 package net.reaper.ancientnature.event;
 
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.Items;
+import net.minecraftforge.client.event.sound.SoundEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,9 +16,10 @@ public class ClickEventHandler {
     public static void onPlayerRightClick(PlayerInteractEvent.RightClickItem event) {
         if (event.getHand() == InteractionHand.MAIN_HAND) { // Check if main hand
             if (event.getItemStack().getItem() == Items.BRUSH) { // Check if holding brush
-                if (event.getEntity().getOffhandItem().getItem() == ModItems.CAMBRIAN_FOSSIL.get()) { // Check if holding fossil in off hand
+                if (event.getEntity().getOffhandItem().getItem() == ModItems.CAMBRIAN_FOSSIL.get()) { // Check if holding fossil in offhand
                     if (Math.random() < 0.3) { // 30% chance
                         event.getEntity().addItem(ModItems.ANOMALOCARIS_FOSSIL.get().getDefaultInstance()); // Give another fossil
+                        event.getEntity().playSound(null, 0, 0, 0, SoundEvent.("cleaned_fossil"), SoundSource.PLAYERS, 1, 1);
                     } else {
                         event.getEntity().getOffhandItem().setCount(event.getEntity().getOffhandItem().getCount() - 1); // Decrease fossil count
                     }
