ed3a61ed9309f92b78ff92a3f2f91c97480e3407
Funado15 <juanjoromo2006@gmail.com>
2024-03-26 10:03:57 +0100

    Deleted CambrianFossilCleanProcedure.java Created ClickEventHandler.java

--- src/main/java/net/reaper/ancientnature/event/ClickEventHandler.java
+++ src/main/java/net/reaper/ancientnature/event/ClickEventHandler.java
@@ -1,6 +1,8 @@
 package net.reaper.ancientnature.event;
 
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -17,7 +19,7 @@ public class ClickEventHandler {
         if (event.getHand() == InteractionHand.MAIN_HAND) { // Check if main hand
             if (event.getItemStack().getItem() == Items.BRUSH) { // Check if holding brush
                 if (event.getEntity().getOffhandItem().getItem() == ModItems.CAMBRIAN_FOSSIL.get())
-                    event.getEntity().playSound(ModSounds.CLEANED_FOSSIL.get(), 1, 1); { // Check if holding fossil in offhand
+                    event.getEntity().playSound(null,0 ,0 ,0 ,ModSounds.CLEANED_FOSSIL.get(), SoundSource.PLAYERS, 1, 1); { // Check if holding fossil in offhand
                     if (Math.random() < 0.15) { // 15% chance
                         event.getEntity().addItem(ModItems.ANOMALOCARIS_FOSSIL.get().getDefaultInstance()); // Give another fossil
 
