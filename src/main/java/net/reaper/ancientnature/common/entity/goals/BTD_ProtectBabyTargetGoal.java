d075b6bcdbc9df0c4f4d0ee1d8ed50596a7c88f4
Marco <st172170@stud.uni-stuttgart.de>
2024-06-07 14:13:11 +0200

    fixed git ignore

--- src/main/java/net/reaper/ancientnature/common/entity/goals/BTD_ProtectBabyTargetGoal.java
+++ src/main/java/net/reaper/ancientnature/common/entity/goals/BTD_ProtectBabyTargetGoal.java
@@ -1,9 +1,7 @@
 package net.reaper.ancientnature.common.entity.goals;
 
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
-import net.minecraft.world.entity.animal.PolarBear;
 import net.reaper.ancientnature.common.entity.BaseTameableDinoEntity;
 import net.reaper.ancientnature.common.entity.ground.OviraptorEntity;
 
